2490544a7cad531eade083c42e39c6f066a841bc
quadrefolio <abdelrahmanelsayed036@gmail.com>
2024-04-30 01:21:19 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Authentication from './components/authentication/Authentication.js'
 import HomePage from './components/HomePage.js';
+import NavigationL from './components/NavigationL.js';
 import Landing from './components/landingpage/Landing.js';
 function App() {
-  
   return (
     <div className="App">
-      <Landing/>
+      <Authentication role="Admin" />
+      {/* <Landing/> */}
+      {/* <HomePage /> */}
+      {/* <NavigationL/> */}
     </div>
   );
 }
