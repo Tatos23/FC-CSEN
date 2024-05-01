4799cc37a888f497f54870f5fee92811f8db6c5c
quadrefolio <abdelrahmanelsayed036@gmail.com>
2024-05-01 14:42:46 +0300

    the edited land page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { ImOpt } from 'react-icons/im';
 import './App.css';
 import Authentication from './components/authentication/Authentication.js'
 import HomePage from './components/HomePage.js';
 import NavigationL from './components/NavigationL.js';
-
+import Landing from './components/landingpage/Landing.js';
 function App() {
   return (
     <div className="App">
-      <Authentication role="Donor" />
-      {/* <Landing/> */}
+    {/* //   <Authentication role="Donor" /> */}
+      <Landing/>
       {/* <HomePage /> */}
       {/* <NavigationL/> */}
     </div>
