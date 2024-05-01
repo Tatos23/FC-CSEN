206b5d5ccec4f49532e2d8aa700377e5af4544d0
abdsh55 <abdullahsherif73+git@gmail.com>
2024-05-02 00:06:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Login from './components/Login.js'
 import HomePage from './components/HomePage.js';
+<<<<<<< Updated upstream
 
 function App() {
   return (
     <div className="App">
       <Login role="Admin" />
+=======
+import NavigationL from './components/NavigationL.js';
+import Landing from './components/landingpage/Landing.js';
+import Hero from './components/Hero.jsx';
+import ORHome from './components/organization/ORHome.js';
+import Navbar from './components/Navbar.jsx';
+function App() {
+  return (
+    <div className="App">
+      {/* <ORHome user = "Adham" role = "Rep"/> */}
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      <Authentication role="Donor" />
+      {/* <Landing /> */}
+>>>>>>> Stashed changes
       {/* <HomePage /> */}
 
     </div>
