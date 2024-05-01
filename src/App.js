5bca7cb4f13e437d49b5b78ff9c55ef32eab7e61
abdsh55 <abdullahsherif73+git@gmail.com>
2024-05-01 21:40:50 +0300

    Add Tailwind

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Authentication from './components/authentication/Authentication.js'
 import HomePage from './components/HomePage.js';
 import NavigationL from './components/NavigationL.js';
 import Landing from './components/landingpage/Landing.js';
+import Hero from './components/Hero.jsx';
 function App() {
   return (
     <div className="App">
-      <Authentication role="Admin" />
-      {/* <Landing/> */}
+      <Hero title="Welcome to the website" subtitle="This is a subtitle" />
+      {/* <Authentication role="Admin" /> */}
+      {/* <Landing /> */}
       {/* <HomePage /> */}
       {/* <NavigationL/> */}
     </div>
