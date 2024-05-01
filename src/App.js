7f5b1f7e001a5b2f223e42715c9e467f31087c5c
quadrefolio <abdelrahmanelsayed036@gmail.com>
2024-05-01 12:10:26 +0300

    edit on my local

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Authentication from './components/authentication/Authentication.js'
 import HomePage from './components/HomePage.js';
 import NavigationL from './components/NavigationL.js';
-import Landing from './components/landingpage/Landing.js';
+
 function App() {
   return (
     <div className="App">
