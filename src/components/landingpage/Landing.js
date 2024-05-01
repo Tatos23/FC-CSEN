fe537d157bc43d255e2504b80652b3bb3896ad11
quadrefolio <abdelrahmanelsayed036@gmail.com>
2024-05-02 00:07:41 +0300

    the edited landing

--- src/components/landingpage/Landing.js
+++ src/components/landingpage/Landing.js
@@ -2,17 +2,18 @@ import './landing.css';
 function Landing(){
 return(
     <>
-        <div className='background-container'><img src='home.jpg' className='homephoto'></img>
-      
+        
+
         <div className='navbar'>
             <div className='leftside'>
                 <img className='logo-navbar'src='logo2.png' alt='logo'></img>
                 <img className='logo-navbar-name'src='name.png' alt='logo'></img>
+                <button className='donate-button' >Donate</button>
             </div>
             <div className='middleside'>
-                <button className='middleside-button'style={{ marginRight: '15%' }}>Home</button>   
+                <button className='middleside-button'style={{ marginRight: '10%' }}>Home</button>   
                 <button className='middleside-button'style={{ marginRight: '1%' }}>Donations</button>
-                <button className='middleside-button'style={{ marginLeft: '10%' }}>AboutUS</button>
+                <button className='middleside-button'style={{ marginLeft: '10%' }}>About Us</button>
                 <button className='middleside-button'style={{ marginLeft: '10%' }}>Categories</button>
             </div>
             <div className='rightside'>
@@ -22,12 +23,18 @@ return(
 
             <button className='searchbutton'><img className='search-icon'src='searchicon.png' alt='logo'></img></button>
             <button className='singin-up' >Login/Register</button>
+            <div className='quote'>"The best way to find yourself is to lose yourself in the service of others."
+            </div>
+
             
             </div>
-            <div className='quote'>The best way to find yourself is to lose yourself in the service of others.</div>
-            
-        </div></div>
+
+           </div>
         
+       <div className='nd'>lallalal</div>
+    
+       
+    
     
     
     
