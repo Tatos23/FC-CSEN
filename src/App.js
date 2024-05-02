18bd265866f72f13c4673056e57a6ce34636e379
abdsh55 <abdullahsherif73+git@gmail.com>
2024-05-02 20:41:27 +0300

    Squashed commit of the following:
    commit 26ce33cca6298e4ca7b9eb894ed195f3a0e99a48
    Author: abdsh55 <abdullahsherif73+git@gmail.com>
    Date:   Thu May 2 20:40:28 2024 +0300
    
        Fix styling in authentication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { ImOpt } from 'react-icons/im';
 import './App.css';
-// import Authentication from './components/authentication/Authentication.js'
+import Authentication from './components/authentication/Authentication.js'
 // import HomePage from './components/HomePage.js';
 // import NavigationL from './components/NavigationL.js';
 // import Landing from './components/landingpage/Landing.js';
-import Authentication from './components/authentication/Authentication.js'
-import HomePage from './components/HomePage.js';
-import NavigationL from './components/NavigationL.js';
-import Landing from './components/landingpage/Landing.js';
-import Hero from './components/Hero.jsx';
+// import Hero from './components/Hero.jsx';
+import AdminHomePage from './components/admin/admin_home_page/AdminHomePage.js';
 function App() {
   return (
     <div className="App">
-      {/* //   <Authentication role="Donor" /> */}
-      <Landing />
+      {/* <AdminHomePage /> */}
+      <Authentication role="Donor" />
+      {/* <Landing /> */}
       {/* <HomePage /> */}
       {/* <NavigationL/> */}
     </div>
