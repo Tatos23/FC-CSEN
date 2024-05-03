f03d0c1660a3dc23ef8d225b9257e13a196c2ead
Abdelrahman24 <abdelrahmanmohamed237070@gmail.com>
2024-05-03 23:44:07 +0300

    adjusted the profile drop down

--- src/components/DropDownProfile.js
+++ src/components/DropDownProfile.js
@@ -2,15 +2,14 @@ import { Link } from 'react-router-dom';
 import './DropDownProfile.css';
 
 const DropDownProfile = () => {
+
+//TODO: functionality of the buttons
     return ( 
         <div className = 'flex flex-col DropDownProfile'>
             <ul className = 'flex flex-col gap-4'>
-                <button onClick={() => handleButtonClick('Profile')}>Profile</button>
-                <button onClick={() => handleButtonClick('Settings')}>Settings</button>
-
-                {/* <Link to={"/home"}>  */}
-                <button onClick={() => handleButtonClick('Logout')}>Logout</button>
-                {/* </Link> */}
+                <button onClick={() => handleButtonClickProfile('Profile')}>Profile</button>
+                <button onClick={() => handleButtonClickSettings('Settings')}>Settings</button>
+                <button onClick={() => handleButtonClickLogout('Logout')}>Logout</button>
             </ul>
         </div>
      );
