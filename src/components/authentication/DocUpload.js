4a0b186d7dfa52706f437527bc479eaecf3cd37f
AhmedWaleedElFar <ahmedw.delfar2002@gmail.com>
2024-05-06 20:25:17 +0300

    Started developing the eta section

--- src/components/authentication/DocUpload.js
+++ src/components/authentication/DocUpload.js
@@ -3,6 +3,13 @@ import './DocUpload.css';
 import React from 'react';
 
 function DocUpload( { role, type }) {
+    const handleFileChange = (e) => {
+        if (e.target.files[0] && e.target.files[0].type !== 'application/pdf') {
+            alert('Only PDF files are allowed');
+            e.target.value = ''; // Clear the selected file
+        }
+    };
+
     return (
         <div className="DocUpload">
             <Header loggedIn={false}/>
@@ -10,7 +17,7 @@ function DocUpload( { role, type }) {
             (
             <div className='DocUpload-Container'>
                 <h1 className="DocUpload-Title">Upload your documents</h1>
-                <input type="file" name="file"  className='DocUpload-document-input'/>
+                <input type="file" name="file"  className='DocUpload-document-input' onChange={handleFileChange}/>
                 <br />
                 <label className='DocUpload-message'>Please upload the documents that can help in the verification process</label>
                 <button type="submit" className='DocUpload-Upload'>Upload</button>
@@ -18,7 +25,7 @@ function DocUpload( { role, type }) {
             :
             (<div className='DocUpload-Container'>
             <h1 className="DocUpload-Title">Upload your documents</h1>
-            <input type="file" name="file"  className='DocUpload-document-input'/>
+            <input type="file" name=""  className='DocUpload-document-input' accept=".pdf" onChange={handleFileChange}/>
             <br />
             <label className='DocUpload-message'>Please upload the documents that can help in your organization verification process</label>
             <button type="submit" className='DocUpload-Upload'>Upload</button>
