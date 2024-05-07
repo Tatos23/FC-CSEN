a5641171725aad207bfe114d9a19698aaeadbed9
Abdelrahman24 <abdelrahmanmohamed237070@gmail.com>
2024-05-07 22:24:23 +0300

    s

--- src/App.js
+++ src/App.js
@@ -12,23 +12,19 @@ import MainTemp from './components/MainTemp.js';
 import Donationselect from './components/donor/Donationselect.js';
 import Header from './components/donor/Header.js';
 import PickUp from './components/donor/PickUp.js';
-import { useEffect } from 'react';
 import DocUpload from './components/authentication/DocUpload.js';
 import EtaSection from './components/donor/EtaSection.js';
 import Blood from './components/organization/Blood.jsx';
 import RequestButtons from './components/organization/RequestButtons.jsx';
 import Book from './components/organization/Book.jsx';
 import Notifications from './components/Notifications.js';
-import DonationsList from './components/donor/DonationsList.js';
 
 function App() {
-  
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-
-          <Route path="/" element={<Landing/>} />
+          <Route path="/" element={<PickUp/>} />
           <Route path="/admin-home" element={<AdminHomePage />} />
           <Route path="/admin-login" element={<Authentication role="Admin" />} />
           <Route path="/donor-login" element={<Authentication role="Donor" />} />
@@ -42,7 +38,6 @@ function App() {
           <Route path="/book" element={<Book />} />
           <Route path="/view-requests" element={<ViewRequests/>} />
           <Route path="/notifications" element={<Notifications/>} />
-
 
         </Routes>
       </BrowserRouter>
