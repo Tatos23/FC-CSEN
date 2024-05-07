22869f57c2f6a33c271b82a764c29d9622408b89
abdsh55 <abdullahsherif73+git@gmail.com>
2024-05-07 23:17:46 +0300

    Implement AdminUsers page
    still missing filteration

--- src/components/admin/admin-users/AdminUsers.js
+++ src/components/admin/admin-users/AdminUsers.js
@@ -1,11 +1,46 @@
+import React, { useState } from 'react';
 import './AdminUsers.css'
 import UserCard from '../components/user_card/UserCard';
-
+import { mockUsers } from '../admin-users-data.js';
 
 function AdminUsers() {
+    const users = mockUsers;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(9);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = users.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(users.length / itemsPerPage);
+
+    const handleClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    }
+
     return (
         <div className='admin-users'>
-            <UserCard type='Donor' name='abdullah' />
+            <div className='admin-users-grid'>
+                {currentItems.map((user, index) => {
+                    if (user.type === 'Donor') {
+                        return <UserCard key={index} type='Donor' name={user.name} />;
+                    } else if (user.type === 'Organization') {
+                        return <UserCard key={index} type='Organization' name={user.name} organizationType={user.organizationType} />;
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+            <div className='pagination'>
+                {currentPage > 3 && <button onClick={() => handleClick(1)}>1</button>}
+                {currentPage > 4 && <span>...</span>}
+                {[...Array(5)].map((page, i) =>
+                    currentPage - 2 + i > 0 && currentPage - 2 + i <= totalPages &&
+                    <button key={i} onClick={() => handleClick(currentPage - 2 + i)}>{currentPage - 2 + i}</button>
+                )}
+                {currentPage < totalPages - 3 && <span>...</span>}
+                {currentPage < totalPages - 2 && <button onClick={() => handleClick(totalPages)}>{totalPages}</button>}
+            </div>
         </div>
     );
 }
--- src/components/admin/components/user_card/UserCard.js
+++ src/components/admin/components/user_card/UserCard.js
@@ -1,10 +1,11 @@
 import './UserCard.css';
 
-export default function UserCard({ type, name }) {
+export default function UserCard({ type, name, organizationType }) {
     return (
         <div className="user-card">
-            <h2 className="user-card-type">Type: {type}</h2>
+            <h2 className="user-card-type">{type}</h2>
             <h1 className="user-card-name">{type == 'Donor' ? 'Username: ' : 'Organization name: '}{name}</h1>
+            {type == 'Organization' ? <h2 className="user-card-name">Organization Type: {organizationType}</h2> : null}
         </div>
     )
 };
