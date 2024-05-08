1904196bc5b73acc6ff99aff5a83094bc9966ef2
abdsh55 <abdullahsherif73+git@gmail.com>
2024-05-08 11:58:46 +0300

    Add filtering in AdminUsers

--- src/components/admin/admin-users/AdminUsers.js
+++ src/components/admin/admin-users/AdminUsers.js
@@ -9,9 +9,11 @@ function AdminUsers() {
     const [itemsPerPage, setItemsPerPage] = useState(9);
     const [filter, setFilter] = useState('All');
     const [searchTerm, setSearchTerm] = useState('');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [selectedTypes, setSelectedTypes] = useState([]);
 
     const filteredUsers = users.filter(user => {
-        const matchesFilter = filter === 'All' || user.type === 'Organization';
+        const matchesFilter = filter === 'All' || (selectedTypes.length === 0 && user.type === 'Organization') || (user.type === 'Organization' && selectedTypes.includes(user.organizationType));
         const matchesSearch = user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
             (user.organizationType && user.organizationType.toLowerCase().includes(searchTerm.toLowerCase()));
         return matchesFilter && matchesSearch;
@@ -27,6 +29,22 @@ function AdminUsers() {
         setCurrentPage(pageNumber);
     }
 
+    const handleTypeSelect = (type) => {
+        setSelectedTypes(prevTypes =>
+            prevTypes.includes(type) ? prevTypes.filter(t => t !== type) : [...prevTypes, type]
+        );
+    }
+
+    const handleDone = () => {
+        setFilter(selectedTypes);
+        setDropdownOpen(false);
+    }
+
+    const handleClear = () => {
+        setSelectedTypes([]);
+        setFilter('All');
+        setDropdownOpen(false);
+    }
 
     return (
         <div className='admin-users'>
@@ -34,9 +52,32 @@ function AdminUsers() {
                 <div className='admin-users-showing'>
                     <span>Show: </span>
                     <button className={filter === 'All' ? 'admin-users-selected' : ''} onClick={() => setFilter('All')}>All</button>
-                    <button className={filter === 'Organization' ? 'admin-users-selected' : ''} onClick={() => setFilter('Organization')}>Organizations</button>
+                    <button className={filter !== 'All' ? 'admin-users-selected' : ''} onClick={() => setFilter('Organization')}>Organizations</button>
                 </div>
                 <input type="text" placeholder="Search..." onChange={e => setSearchTerm(e.target.value)} />
+                <div className='admin-users-dropdown'>
+                    <button className={selectedTypes.length !== 0 ? 'admin-users-dropdown-selected' : 'admin-users-dropdown-button'} onClick={() => setDropdownOpen(!dropdownOpen)}>Filter by Type</button>
+                    {dropdownOpen && (
+                        <div className='admin-users-dropdown-menu'>
+                            <div className='admin-users-dropdown-menu-types'>
+                                {['Health', 'Blood'].map(type => (
+                                    <div className='admin-users-dropdown-menu-type' key={type}>
+                                        <label>{type}</label>
+                                        <input
+                                            type='checkbox'
+                                            checked={selectedTypes.includes(type)}
+                                            onChange={() => handleTypeSelect(type)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <div className='admin-users-dropdown-menu-buttons'>
+                                <button className='admin-users-dropdown-menu-buttons-1' onClick={handleDone}>Done</button>
+                                <button className='admin-users-dropdown-menu-buttons-2' onClick={handleClear}>Clear All</button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
 
             <div className='admin-users-grid'>
@@ -55,7 +96,13 @@ function AdminUsers() {
                 {currentPage > 4 && <span>...</span>}
                 {[...Array(5)].map((page, i) =>
                     currentPage - 2 + i > 0 && currentPage - 2 + i <= totalPages &&
-                    <button key={i} onClick={() => handleClick(currentPage - 2 + i)}>{currentPage - 2 + i}</button>
+                    <button
+                        key={i}
+                        onClick={() => handleClick(currentPage - 2 + i)}
+                        className={currentPage - 2 + i === currentPage ? 'admin-users-selected' : ''}
+                    >
+                        {currentPage - 2 + i}
+                    </button>
                 )}
                 {currentPage < totalPages - 3 && <span>...</span>}
                 {currentPage < totalPages - 2 && <button onClick={() => handleClick(totalPages)}>{totalPages}</button>}
