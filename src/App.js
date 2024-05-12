f0acdd39e6ef6b46d7580c946e1524d9f7f6cbf6
Abdelrahman24 <abdelrahmanmohamed237070@gmail.com>
2024-05-12 22:43:57 +0300

    added the users db.json

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
           
         <Route index element={<Landing/>} />
             
-          <Route path="/admin-dashboard" element={<AdminDashBoard />} >
+          <Route path="/admin-dashboard" element={<Donationselect />} >
             <Route index element={<AdminHomePage />} />
             <Route path="system-users" element={<AdminUsers />} />
             <Route path="registering-users" element={<RegisteringUsers />} />
